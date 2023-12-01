a3ce4a19cddefce40dab016cb3481612854c3060
swathichowdary123 <swathichowdary548.com>
2023-12-01 10:32:49 +0530

    complete assignment on DOM manipulation

--- block-BJaadz/code/script.js
+++ block-BJaadz/code/script.js
@@ -1,11 +1,11 @@
 // Select the h1 element and store it in a variable named heading.
-
+let heading=document.querySelector('h1');
 // Check the typeof heading and log it.
-
+console.log(typeof heading);
 // Change the color of heading to black.
-
+document.heading="black";
 // Select all the hr elements and store it in a variable named allHrs using querySelectorAll
-
+let allHrs=document.querySelectorAll('hr');
 /* 
 Convert the NodeList returned by querySelectorAll to Array using Array.from() or spread operator and store it in allHrsArray
 
@@ -16,19 +16,20 @@ let allHrsArray = Array.from(allHrs)
 */
 
 // Set the border of the all the hr elements to "1px solid tomato"
-
+allHrs.style.border="1px solid tomato";
 // Change the background of all the hr to "antiquewhite" using for of loop.
 
 // Change the 'border-radius' of all the hr to "5px" using array.
 
 // Change the alignment of the heading(h1) to center.
-
+heading.style.textAlign="center";
 // Change the font size of the heading to 3rem.
-
+heading.style.fontSize="3rem";
 // Change the border of hr with class 'image' to `2px solid purple`.
-
+allHrs.getAttribute('image').border="2px solid purple";
 // Hide the box number 17 (last box).
-
+let lastBox=document.querySelector('box seventeen');
+lastBox.remove();
 // Change the border of all the hr element from solid to dashed type
 
 // Create a pragraph element and store it in variable named 'para' using `createElement`
